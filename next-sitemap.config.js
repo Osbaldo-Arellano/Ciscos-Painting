b243b02bc58c1f42d28a07f539c467ade03c6658
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2025-06-25 16:25:29 -0700

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://ciscospainting.com',
-  generateRobotsTxt: true, // Generates both sitemap.xml and robots.txt
-  sitemapSize: 5000,
-  changefreq: 'weekly',
+  siteUrl: 'https://ciscos-painting.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'monthly',
   priority: 0.7,
-  exclude: ['/404', '/admin'],
-  robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
-  },
+  sitemapSize: 5000,
 };
